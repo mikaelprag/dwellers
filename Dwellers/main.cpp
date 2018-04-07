d77d3258a1acb74f661a2d3c669d3022de40bb25
Mikael <miccet@yiratal.local>
2018-04-07 16:14:52 +0200

    Initial Commit

---
+++ Dwellers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dwellers
+//
+//  Created by Mikael on 07/04/2018.
+//  Copyright © 2018 Mikael. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
